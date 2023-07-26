2a69a71f1edbf63141727d41f8fe0c624bc6a9b9
Andrew Wilborn <andrew.j.wilborn@sbcglobal.net>
2023-07-26 12:03:00 -0400

    uploading to storage

--- app/page.js
+++ app/page.js
@@ -1,14 +1,35 @@
 "use client"
-
 import { useState } from "react"
+import { initializeApp } from "firebase/app"
+import { getStorage, ref, uploadBytes } from "firebase/storage"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBnB2V7F1RQltugOrxioCpToNjCfX5oTp0",
+  authDomain: "my-first-firestore-ajw.firebaseapp.com",
+  projectId: "my-first-firestore-ajw",
+  storageBucket: "my-first-firestore-ajw.appspot.com",
+  messagingSenderId: "102677368984",
+  appId: "1:102677368984:web:0ba04221481f65702300ed"
+};
 
 export default function Home() {
 
   const [file, setFile] = useState()
+  const [uploadedFile, setUploadedFile] = useState()
 
   const handleFile = (e) => {
-    console.log(e.target.files[0])
     setFile(e.target.files[0])
+    const app = initializeApp(firebaseConfig) // connects to our project
+    const storage = getStorage(app) // connects to storage
+
+    const filename = e.target.files[0].name
+    const imageRef = ref(storage, 'images/' + filename)
+
+    const url = `https://firebasestorage.googleapis.com/v0/b/my-first-firestore-ajw.appspot.com/o/images%2F${filename}?alt=media`
+
+    uploadBytes(imageRef, e.target.files[0])
+      .then(() => setUploadedFile(url))
+      .catch(alert)
   }
 
   return (
@@ -17,8 +38,14 @@ export default function Home() {
       <input type="file" accept="image/*" onChange={handleFile} />
       {file &&
         <>
-          <p>{file.name}</p>
+          <p>Image from computer</p>
           <img src={URL.createObjectURL(file)}/>
+        </>
+      }
+      {uploadedFile &&
+        <>
+          <p>Image from storge</p>
+          <img src={uploadedFile}/>
         </>
       }
     </main>
